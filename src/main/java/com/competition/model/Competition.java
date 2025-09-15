6c063eb63c8580b36c0bd4d40cb236b664d55146
HuaTiHui <usahuatihui@gmail.com>
2025-09-15 00:30:10 -0700

    Update Competition.java

--- src/main/java/com/competition/model/Competition.java
+++ src/main/java/com/competition/model/Competition.java
@@ -1,18 +1,24 @@
 package com.example.competition.model;
 
+import java.util.List;
+
 public class Competition {
     private int id;
     private String name;
     private String date;
     private String location;
+    private String sport;
+    private List<Team> teams;
+    private Result result;
 
     public Competition() {}
 
-    public Competition(int id, String name, String date, String location) {
+    public Competition(int id, String name, String date, String location, String sport) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.location = location;
+        this.sport = sport;
     }
 
     public int getId() { return id; }
@@ -26,4 +32,13 @@ public class Competition {
 
     public String getLocation() { return location; }
     public void setLocation(String location) { this.location = location; }
+
+    public String getSport() { return sport; }
+    public void setSport(String sport) { this.sport = sport; }
+
+    public List<Team> getTeams() { return teams; }
+    public void setTeams(List<Team> teams) { this.teams = teams; }
+
+    public Result getResult() { return result; }
+    public void setResult(Result result) { this.result = result; }
 }
